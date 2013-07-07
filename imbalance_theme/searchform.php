152073d21d73c2061739ff096264e30dcf380a10
KolevDarko <kolevdarko@gmail.com>
2013-07-07 22:58:37 +0200

    кирилично

--- imbalance_theme/searchform.php
+++ imbalance_theme/searchform.php
@@ -1,3 +1,3 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-    <input type="text" onfocus="if(this.value=='its a great way to find information') this.value='';" onblur="if(this.value=='') this.value='its a great way to find information';" value="its a great way to find information" name="s" id="s" /><br />
+    <input type="text" onfocus="if(this.value=='Најбргу до информациите') this.value='';" onblur="if(this.value=='') this.value='Најбргу до информациите';" value="Најбргу до информациите" name="s" id="s" /><br />
 </form>
